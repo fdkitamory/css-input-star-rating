d77409533c61b30ddde61e6bf828ff3303eaf572
Nikita Konoplin <franklin.d.kitamory@gmail.com>
2014-07-18 10:07:58 +0700

    Add files =)

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// $(document).ready(function(){
+// 	var i = 2,
+// 		slidesCount = $('#slide > :radio').size();
+// 	setInterval(function () {
+// 		if (i <= slidesCount){
+// 			$('#slide > input#slide_'+i).click();	
+// 			i++;
+// 		}else{
+// 			i = 1;
+// 			$('#slide > input#slide_'+i).click();
+// 			i++;
+// 		}
+		
+// 	}, 7000);
+// });
